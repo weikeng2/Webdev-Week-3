79e3adf988e8d30c31246d1da1180b472b0cdf88
weikeng2 <42951918+weikeng2@users.noreply.github.com>
2018-09-23 16:58:21 -0500

    hgjghj

---
+++ index.js
@@ -0,0 +1,15 @@
+function multiply3(numberlist) {
+    for (let i = 0; i < numberlist; i++) {
+        numberlist[i] *= 3
+    }
+    return numberlist
+}
+
+function concantenator(stringlist) {
+    let output = ""
+    for (let i = 0; i < stringlist; i++) {
+        output += stringlist[i]
+    }
+    return output;
+  }
+  
